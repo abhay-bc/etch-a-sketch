8927502c907c34244941be12bac9a3ad324e2a03
Abhaya BC <abhaybc000@gmail.com>
2022-09-28 16:45:51 +0545

    reinstallation

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+
+let sqDiv;
+for(let i = 0; i < 256; i++) {
+    sqDiv = document.createElement('div');
+    sqDiv.setAttribute('class', 'sq-div');
+    container.appendChild(sqDiv);
+    sqDiv.addEventListener('mouseover', ()=>{
+        sqDiv.style['background-color'] = randColor();
+    })
+}
+
+function rand(a) {
+    let c = Math.floor(Math.random()*a)
+    return c;
+}
+
+function randColor(){
+    let color = `rgb(${rand(255)}, ${rand(255)}, ${rand(255) })`
+    return color;
+}
