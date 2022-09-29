f90eeb2efa391f5231b7650118c87a8b84dff487
abhaya-bc <abhaybc000@gmail.com>
2022-09-29 12:29:30 +0545

    I don't know what is happening

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-const container = document.querySelector('.container');
-
-let sqDiv;
-for(let i = 0; i < 256; i++) {
-    sqDiv = document.createElement('div');
-    sqDiv.setAttribute('class', 'sq-div');
-    container.appendChild(sqDiv);
-    sqDiv.addEventListener('mouseover', (e)=>{
-        e.target.style['background-color'] = randColor();
-    })
+window.onload = ()=> {
+    newlayout();
 }
+
+const container = document.querySelector('.container');
 
 function rand(a) {
     let c = Math.floor(Math.random()*a)
@@ -20,3 +14,41 @@ function randColor(){
     return color;
 }
 
+function removelayout() {
+    let child = document.querySelectorAll('.sq-div')
+    for(let i = 0; i < child.length; i++) {
+        child[i].style['background-color'] =  'white';
+    }
+}
+
+function newSize() {
+    let square = prompt('Number of square (range 64 to 100)');
+    let val = parseInt(square);
+    if (isNaN(val)) {
+        newlayout();
+    } else {
+        if (val < 16 || val > 100) {
+            alert('Invalid Selection');
+            newSize();
+        } else {
+            newlayout(val);
+        }
+    }
+}
+
+function newlayout(numOfsq=16) { 
+    const contWidth = 600;
+    let childWidth = contWidth / numOfsq;
+    let squared = numOfsq * numOfsq;
+
+    for(let i = 0; i < squared; i++) {
+        let sqDiv = document.createElement('div');
+            sqDiv.setAttribute('class', 'sq-div');
+            sqDiv.style.height = `${childWidth}px`;
+            sqDiv.style.width = `${childWidth}px`;
+
+        container.appendChild(sqDiv);
+        sqDiv.addEventListener('mouseover', (e)=>{
+            e.target.style['background-color'] = 'black';
+        });}  
+}
